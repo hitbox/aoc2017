882fb42daf19c33f344b0fc65c344b315b99bc0d
Carl Harris <elgoogemail2007@gmail.com>
2017-12-19 04:27:43 -0500

    day 7, 8, 9, and 10 down

--- d07/main.py
+++ d07/main.py
@@ -1,19 +1,37 @@
+import collections
 import os
 import re
 import textwrap
 
 from pprint import pformat, pprint as pp
 
+def oddity(iterable, key=None):
+    if key is None:
+        key = lambda x: x
+
+    no_odd = (None, None)
+    summary = collections.Counter(map(key, iterable))
+
+    if len(summary) == 2:
+        common, different = summary.items()
+        if different[1] == common[1]:
+            return no_odd
+
+        if different[1] > common[1]:
+            common, different = different, common
+        rv = (common[0], different[0])
+        return rv
+
+    return no_odd
+
 class Tower:
 
     def __init__(self):
         self.bottom = None
-        self.tower = None
-        self.weights = None
-
-    def parse(self, inputstr):
         self.tower = {}
         self.weights = {}
+
+    def parse(self, inputstr):
         line_re = re.compile("(?P<name>[a-z]+) \((?P<weight>\d+)\)(?: \-> )?(?P<holding>.*)")
         for line in inputstr.splitlines():
             match = line_re.match(line)
@@ -39,16 +57,39 @@ class Tower:
             if name == self.bottom:
                 continue
             weight = self.weights[name]
-            subweight = sum(self.get_program_weights(name))
+            subweight = sum(w for p, w in self.get_program_weights(name))
             yield (name, weight, subweight)
 
-    def get_program_weights(self, start):
-        if start not in self.tower:
-            return
-        for holding in self.tower[start]:
-            yield self.weights[holding]
+    def get_program_weights(self, start=None):
+        if start is None:
+            start = self.bottom
+        for holding in self.tower.get(start, ()):
+            yield holding, self.weights[holding]
             if holding in self.tower:
                 yield from self.get_program_weights(holding)
+
+    def get_odd_program(self, program=None, goal=0):
+        if program is None:
+            program = self.bottom
+
+        weights = {}
+        for child in self.tower.get(program, ()):
+            weights[child] = self.weights[child] + sum(w for p, w in self.get_program_weights(child))
+        common, odd = oddity(weights.values())
+
+        if odd:
+            for child, weight in weights.items():
+                if weight == odd:
+                    return self.get_odd_program(child, common)
+        else:
+            # all children same, this program is the problem
+            return program, sum(weights.values()), goal
+
+    def solution(self):
+        program, subweight, goal = self.get_odd_program()
+        weight = self.weights[program]
+
+        return weight + (goal - weight - subweight)
 
     def find_balanced_weight(self):
         totals = {}
@@ -62,16 +103,6 @@ class Tower:
             if count > 1:
                 return total
 
-    def find_wrong_weight(self, root):
-        for child in self.tower[root]:
-            if child in self.tower:
-                self.find_wrong_weight(child)
-            else:
-                print(child)
-                subweights = list(self.get_program_weights(child))
-                if len(set(subweights)) > 1:
-                    print(subweights)
-
     def find_corrected_weight(self):
         balanced_weight = self.find_balanced_weight()
         for name, weight, subweight in self._iter_weighted():
@@ -83,6 +114,10 @@ class Tower:
                 return weight + correction
 
 def tests():
+    assert oddity("a") == (None, None)
+    assert oddity("ab") == (None, None)
+    assert oddity("baa") == ("a", "b")
+
     inputstr = "".join(textwrap.dedent("""\
                 pbga (66)
                 xhth (57)
@@ -101,6 +136,8 @@ def tests():
     tower.parse(inputstr)
     assert tower.bottom == "tknk"
     assert tower.find_corrected_weight() == 60
+
+    assert tower.solution() == 60
 
     # test with recursion
     inputstr = "".join(textwrap.dedent("""\
@@ -121,7 +158,7 @@ def tests():
     tower = Tower()
     tower.parse(inputstr)
     name = "fwft"
-    assert tower.weights[name] + sum(tower.get_program_weights(name)) == 370
+    assert tower.weights[name] + sum(w for p, w in tower.get_program_weights(name)) == 370
 
 def main():
     tests()
@@ -132,8 +169,16 @@ def main():
 
     print("part 1: %s" % tower.bottom)
 
-    tower.find_wrong_weight(tower.bottom)
+    # 771 is too low
+    # 960 is too low
+    # 2300 is too high
+    #
     #print("part 2: %s" % tower.find_corrected_weight())
+    print("part 2: %s" % tower.solution())
+    # Got fed up with this, cheated looking at Ned Batchelder's solution, still
+    # had problems, his "My children are balanced, I'm the problem" helped me a
+    # lot.
+    # Leaving the code a mess.
 
 if __name__ == "__main__":
     main()
